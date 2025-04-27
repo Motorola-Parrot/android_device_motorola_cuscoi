7357559d836ea9112ddcd5ca2b7a08c3208dbc0c
Rakhshan Ali <alirakhshan7070@gmail.com>
2025-04-27 21:25:27 +0530

    cuscoi: Add BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD flag
    - Also update recovery modules

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,6 +26,7 @@ TARGET_KERNEL_CONFIG += vendor/lineage_cuscoi.config
 BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load))
 BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE := $(DEVICE_PATH)/modules.blocklist
 BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load.recovery))
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load.ramdisk))
 BOOT_KERNEL_MODULES := $(BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD)
 
 # Partitions
