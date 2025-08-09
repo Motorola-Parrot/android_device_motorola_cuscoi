c2a8314c8a5f4d31ecd7e652b7e403f8fc53db13
Rakhshan Ali <alirakhshan7070@gmail.com>
2025-08-09 09:16:53 +0530

    cuscoi: Add build fingerprint
    Change-Id: If60be1782ccd6fe18abaeeb87530ee156e01a667

--- lineage_cuscoi.mk
+++ lineage_cuscoi.mk
@@ -31,3 +31,8 @@ PRODUCT_BRAND := motorola
 PRODUCT_MODEL := motorola edge 50 fusion
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="cuscoi_g-user 15 V1UUI35M.15-41-6 b33d6 release-keys" \
+    BuildFingerprint=motorola/cuscoi_g/cuscoi:15/V1UUI35M.15-41-6/b33d6:user/release-keys \
+    DeviceProduct=cuscoi_g
